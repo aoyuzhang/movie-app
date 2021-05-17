820039d625bf27f909fe0785edd66a7e5b189774
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-17 15:24:34 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
   const getMovieRequest = async () => {
     const url = 'http://www.omdbapi.com/?s=star wars&apikey=263d22d8';
     const response = await fetch(url);
@@ -20,7 +24,11 @@ const App = () => {
   }, []);
 
 	return (
-		<div className='container-fluid movie-app'>
+    <div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
 			<div className='row'>
 				<MovieList movies={movies} />
 			</div>
