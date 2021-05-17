c678b80493c9ee053d98a2d04b6fab9ebc1ff87e
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-17 15:36:35 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,24 +7,25 @@ import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+	const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async () => {
-    const url = 'http://www.omdbapi.com/?s=star wars&apikey=263d22d8';
-    const response = await fetch(url);
-    const responseJson = await response.json();
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-    if(responseJson.Search){
-      setMovies(responseJson.Search);
-    }
-  };
+		const response = await fetch(url);
+		const responseJson = await response.json();
 
-  useEffect(() => {
-    getMovieRequest();
-  }, []);
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
 	return (
-    <div className='container-fluid movie-app'>
+		<div className='container-fluid movie-app'>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
 				<MovieListHeading heading='Movies' />
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
