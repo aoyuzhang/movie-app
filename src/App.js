99bbcfcbb9b07da27c38d86d107ac45e5680c672
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-18 03:17:34 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
 	const [searchValue, setSearchValue] = useState('');
+  const [favourites, setFavourites] = useState([]);
 
 	const getMovieRequest = async (searchValue) => {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
@@ -20,6 +23,11 @@ const App = () => {
 		}
 	};
 
+  const addFavouriteMovie = (movie) =>{
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  }
+
 	useEffect(() => {
 		getMovieRequest(searchValue);
 	}, [searchValue]);
@@ -31,8 +39,12 @@ const App = () => {
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
 			<div className='row'>
-				<MovieList movies={movies} />
-			</div>
+        <MovieList
+          movies = {movies}
+          favouriteComponent = {AddFavourites}
+          handleFavouritesClick = {addFavouriteMovie}
+        />
+      </div>
 		</div>
 	);
 };
