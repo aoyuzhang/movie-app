d207c59a409dbeac0cd9bf92074cc0e68bc03419
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-18 04:53:25 -0700

    update

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ const App = () => {
 		getMovieRequest(searchValue);
 	}, [searchValue]);
 
-	// useEffect(() => {
-	// 	const movieFavourites = JSON.parse(
-	// 		localStorage.getItem('react-movie-app-favourites')
-	// 	);
-	//
-	// 	setFavourites(movieFavourites);
-	// }, [favourites]);
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		setFavourites(movieFavourites);
+	}, [favourites]);
 
 	const saveToLocalStorage = (items) => {
 		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
